d75056493587c5e21299d870c2fe7aa2f5bc014c
Fehlipe19 <168611944+Fehlipe19@users.noreply.github.com>
2025-11-01 22:27:42 -0400

    Rotate list right by amount

--- week01/code/Arrays.cs
+++ week01/code/Arrays.cs
@@ -1,3 +1,4 @@
+using System.Diagnostics;
 public static class Arrays
 {
     /// <summary>
@@ -13,7 +14,15 @@ public static class Arrays
         // step by step before you write the code. The plan should be clear enough that it could
         // be implemented by another person.
 
-        return []; // replace this return statement with your own
+        //Iterate from 1 to length and multiply number to loop index and add each result to the list. 
+        double[] multiples = new double[length];
+
+        for (int i = 1; i <= length; i++)
+        {
+            multiples[i-1] = number * i;
+        }
+
+        return multiples; // replace this return statement with your own
     }
 
     /// <summary>
@@ -29,5 +38,28 @@ public static class Arrays
         // Remember: Using comments in your program, write down your process for solving this problem
         // step by step before you write the code. The plan should be clear enough that it could
         // be implemented by another person.
+
+        // First I'll getRange from beginnig to amount, then List slice data from amount to data.Count 
+        // minus amount. Finally add the list slice to the end of the list getRange created.
+
+        List<int> newArrayEndingPart = data.GetRange(0, data.Count - amount);
+        List<int> newArrayBeginning = data.GetRange(data.Count - amount, amount);
+
+        newArrayBeginning.AddRange(newArrayEndingPart);
+
+        // for (int i = 0; i < data.Count; i++)
+        // {
+            // Debug.WriteLine(newArrayBeginning[i]);
+            // Debug.WriteLine(newArrayBeginning[i]);
+            // Debug.WriteLine(newArrayEndingPart[i]);
+        // }
+
+        data.Clear();
+        data.AddRange(newArrayBeginning);
+
+        // for (int i = 0; i < data.Count; i++)
+        // {
+        //     Debug.WriteLine(data[i]);
+        // }
     }
 }
